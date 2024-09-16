d80461c68a07d1edce92be8575b817283ee38c2e
RafliPernanda <raflipernanda212@gmail.com>
2024-09-16 21:35:33 +0800

    Commit

---
+++ Post-Test/Post-Test-2/2409106040_MuhammadRafliPernanda_A2'24_posttest2.py
@@ -0,0 +1,17 @@
+#Input Nama, Nim, dan Harga beras
+NamaLengkap = str(input("Masukkan Nama Lengkap Anda: "))
+NIM = int(input("Masukkan NIM Anda: "))
+HargaBeras = int(input("Berapa harga beras yang ingin anda beli? "))
+
+#Output Nama, Nim, dan Harga Beras
+print(NamaLengkap, "dengan NIM", NIM, "ingin membeli beras seharga Rp.", HargaBeras)
+
+#Menghitung harga dari beras setelah diskon
+hargaMawar = HargaBeras - (HargaBeras * 0.11)
+hargaSania = HargaBeras - (HargaBeras * 0.14)
+hargaMaknyus = HargaBeras - (HargaBeras * 0.17)
+
+#Menampilkan harga dari setiap jenis beras setelah diskon
+print("Jika Anda membeli beras Mawar, Anda harus membayar Rp." , hargaMawar , " Setelah mendapat diskon 11%")
+print("Jika Anda membeli beras Sania, Anda harus membayar Rp." , hargaSania , " Setelah mendapat diskon 14%")
+print("Jika Anda membeli beras Maknyus, Anda harus membayar Rp." , hargaMaknyus , " Setelah mendapat diskon 17%")
